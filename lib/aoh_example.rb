59ad751d7dcd5178d1bb497be0f7d6451e820bf3
calm-collection-3916 <khalinredd@gmail.com>
2020-12-31 20:14:00 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,11 +22,11 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   literal_array_hash = [ 
-    DON_G.sort,
-    JOELLE_VD.sort,
-    PAT_M,sort,
-    KATE_G.sort,
-    BRUCE_G.sort
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
     ]
 end
 
